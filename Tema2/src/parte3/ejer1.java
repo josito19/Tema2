a29c0b34791b5d8333d8c967e3d7554f16380e8d
Administrator <Administrator@WIN-6FBU89FHBJR>
2024-10-11 04:22:46 +0200

    Ejercicios

---
+++ Tema2/src/parte3/ejer1.java
@@ -0,0 +1,70 @@
+package parte3;
+
+import java.util.Scanner;
+
+public class ejer1 {
+
+	public static void main(String[] args) {
+//Creamos el escáner y las variables
+		Scanner sc = new Scanner(System.in);
+		int n;
+		int mil;
+		int cien;
+		int diez;
+		int u;
+
+//Solicitamos el número y lo asignamos a la variable		
+		System.out.println("Introduca un número del 0 al 9999:");
+		n = sc.nextInt();
+		mil = n / 1000;
+		cien = (n / 100) % 10;
+		diez = (n / 10) % 10;
+		u = n % 10;
+		if (n >= 0 && n <= 9999) {
+			if (n >= 0 && n < 10) {
+				System.out.println("Un número de una cifra siempre será capicúo");
+			} else {
+				if (n > 9 && n < 100) {
+
+					switch (n) {
+					case 11 -> System.out.println("Es capicúa");
+					case 22 -> System.out.println("Es capicúa");
+					case 33 -> System.out.println("Es capicúa");
+					case 44 -> System.out.println("Es capicúa");
+					case 55 -> System.out.println("Es capicúa");
+					case 66 -> System.out.println("Es capicúa");
+					case 77 -> System.out.println("Es capicúa");
+					case 88 -> System.out.println("Es capicúa");
+					case 99 -> System.out.println("Es capicúa");
+					default -> System.out.println("No es capicúa");
+					}
+
+				} else {
+					if (n > 99 && n < 1000) {
+
+						if (cien == u) {
+							System.out.println("Es capicúa");
+						} else {
+							System.out.println("No es capicúa");
+						}
+
+					} else {
+						if (n > 999 && n < 10000 && mil == u && diez == cien) {
+							System.out.println("Es capicúa");
+						} else {
+							System.out.println("No es capicúa");
+						}
+					}
+
+				}
+
+			}
+
+		} else {
+			System.out.println("Número incorrecto.");
+		}
+
+		sc.close();
+	}
+
+}
