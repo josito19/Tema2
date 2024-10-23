f22c2431495e282e885af61c2690f71cc38d9e1d
Administrator <Administrator@WIN-6FBU89FHBJR>
2024-10-24 00:06:47 +0200

    Ejercicios for

--- Tema2/src/parte6/ejer8.java
+++ Tema2/src/parte6/ejer8.java
@@ -5,31 +5,28 @@ import java.util.Scanner;
 public class ejer8 {
 
 	public static void main(String[] args) {
+		//Creamos el escáner y las dos variables
 		Scanner sc = new Scanner(System.in);
+		//a será el primer número
 		int a;
+		//b será el segundo
 		int b;
-
+		//Solicitamos los números y los asignamos con el escáner
 		System.out.println("Introduce dos números:");
 		a = sc.nextInt();
 		b = sc.nextInt();
 
-		// Si A es menor que B
+		// Si A es menor que B se mostrará i desde a hasta b
 		if (a < b) {
 			for (int i = a; i <= b; i++) {
 				System.out.println(i);
 			}
 		}
-		// Si B es menor que A
+		// Si B es menor que A se mostrará i desde b hasta a
 		else if (b < a) {
 			for (int i = b; i <= a; i++) {
 				System.out.println(i);
 			}
-		}
-		// Si A y B son iguales, mostrar uno de los dos
-		else {
-			System.out.println("Los números son iguales: " + a);
-		}
-
 		sc.close();
 	}
 }
